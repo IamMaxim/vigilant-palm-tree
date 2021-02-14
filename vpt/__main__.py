c22634cde3a1fcdd43bb1441c806126cb8ebad56
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-14 14:23:50 +0300

    Expose audio sources listing functionality to CLI

--- vpt/__main__.py
+++ vpt/__main__.py
@@ -1,4 +1,11 @@
 '''Main entry point for the package'''
 from vpt.cli import parse_args
+from vpt.sources import print_audio_inputs
 
-print(parse_args())
+args = parse_args()
+
+if args['cmd'] == 'sources':
+    if args['source'] == 'audio':
+        print_audio_inputs()
+    elif args['source'] == 'video':
+        pass
