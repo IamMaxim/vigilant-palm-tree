f74a8a56cec581c3224dc2f75cbb6dd309b6fcc9
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-24 17:58:48 +0300

    Rename functions to match architecture

--- vpt/sinks/file_store/__init__.py
+++ vpt/sinks/file_store/__init__.py
@@ -21,26 +21,26 @@ class FileStore(SinkBase):
         self.dir = Path(dir_name)
 
         self.mouse_file = open(self.dir / 'mouse_output.txt', 'w+', buffering=1)
-        mouse_source.get_data_stream().subscribe(self._process_mouse)
+        mouse_source.get_data_stream().subscribe(self.store_mouse_event)
 
         self.keyboard_file = open(self.dir / 'keyboard_output.txt', 'w+', buffering=1)
-        keyboard_source.get_data_stream().subscribe(self._process_key)
+        keyboard_source.get_data_stream().subscribe(self.store_key_event)
 
         with contextlib.suppress(FileNotFoundError):
             os.remove(self.dir / 'audio.wav')
         self.audio_file = sf.SoundFile(self.dir / 'audio.wav',
                                        mode='w', samplerate=44100, channels=1)
-        audio_source.get_data_stream().subscribe(self._process_audio)
+        audio_source.get_data_stream().subscribe(self.store_audio_frame)
 
-    def _process_audio(self, frame: np.ndarray):
+    def store_audio_frame(self, frame: np.ndarray):
         '''Write the audio frame to its file'''
         self.audio_file.write(frame)
 
-    def _process_key(self, event: keyboard.KeyboardEvent):
+    def store_key_event(self, event: keyboard.KeyboardEvent):
         '''Write the keyboard event to its file'''
         self.keyboard_file.write(f'{event.event_type} {event.name} {event.scan_code} {event.time}\n')
 
-    def _process_mouse(self, event: Union[mouse.ButtonEvent, mouse.MoveEvent, mouse.WheelEvent]):
+    def store_mouse_event(self, event: Union[mouse.ButtonEvent, mouse.MoveEvent, mouse.WheelEvent]):
         '''Write the mouse event to its file'''
         if isinstance(event, mouse.ButtonEvent):
             self.mouse_file.write(f'button {event.button} {event.event_type} {event.time}\n')
