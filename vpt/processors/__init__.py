58aff8a9a80008aed22a4ee99ac64565d5560575
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-24 17:58:35 +0300

    Add missing imports (exports)

--- vpt/processors/__init__.py
+++ vpt/processors/__init__.py
@@ -1,5 +1,6 @@
 '''A collection of the processor node that operate on the data.'''
 from .engagement_estimator import EngagementEstimator
 from .gaze_detector import GazeDetector
+from .gaze_engagement_estimator import GazeEngagementEstimator
 from .mouse_compressor import MouseCompressor
 from .speech_detector import SpeechDetector
--- vpt/sinks/__init__.py
+++ vpt/sinks/__init__.py
@@ -1,3 +1,4 @@
 '''A collection of sink nodes.'''
 from .file_store import FileStore
 from .video_display import VideoDisplay
+from .sqlite_store import SQLiteStore
