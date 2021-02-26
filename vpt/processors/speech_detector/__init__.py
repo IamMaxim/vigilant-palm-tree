df45da4ec21c2ad85199dafa45c9bdfba4286506
Lev Chelyadinov <leva181777@gmail.com>
2021-02-26 22:20:54 +0500

    Clean up implementation, track mean loudness

--- vpt/processors/speech_detector/__init__.py
+++ vpt/processors/speech_detector/__init__.py
@@ -11,15 +11,20 @@ from vpt.processors.base import ProcessorBase
 VOICE_RANGE = range(80, 1100 + 1)
 # The threshold which the amplitude needs to overcome to be considered present (in undefined units).
 NOISE_THRESHOLD = .5
+SILENCE_THRESHOLD = .3
 
 
 class SpeechDetector(ProcessorBase[bool]):
     '''Detects speech in an audio stream and outputs a signal stream for that.'''
     subj = Subject()
+    mean_loudness: np.float64
+    _samples: int
 
     def __init__(self, audio_source: SourceBase[np.ndarray]):
         '''Wire up the detector to an arbitrary audio source.'''
         audio_source.get_data_stream().subscribe(self.detect_speech)
+        self.mean_loudness = None
+        self._samples = 0
 
     def get_data_stream(self):
         return self.subj
@@ -28,17 +33,46 @@ class SpeechDetector(ProcessorBase[bool]):
         '''Detect sound in speech frequencies in a given audio frame.'''
         if frame.ndim == 1:
             frame = frame.reshape(len(frame), 1)
-        self.subj.on_next(np.any(self.fft(frame.mean(axis=1))[VOICE_RANGE] >= NOISE_THRESHOLD))
+
+        freqs = self.fft(frame.mean(axis=1))
+        this_loudness = freqs.max(axis=0)
+
+        if self.is_silence(this_loudness):
+            self.subj.on_next(False)
+        else:
+            freqs_norm = self.normalize_frequencies(freqs)
+            self.subj.on_next(np.any(freqs_norm[VOICE_RANGE] >= NOISE_THRESHOLD))
+        self.update_mean(this_loudness)
+
+    def update_mean(self, new_loudness):
+        '''Updates the internal mean relative loudness level with a new sample.'''
+        if self.mean_loudness is None:
+            self.mean_loudness = new_loudness
+        else:
+            self.mean_loudness *= self._samples / (self._samples + 1)
+            self.mean_loudness += new_loudness / (self._samples + 1)
+        self._samples += 1
+
+    def is_silence(self, new_loudness):
+        '''Tests if a given relative loudness level is quiet enough to be disregarded.'''
+        if self.mean_loudness is None:
+            return False
+        try:
+            return new_loudness / self.mean_loudness < SILENCE_THRESHOLD
+        except ZeroDivisionError:
+            return True
 
     @staticmethod
     def fft(frame):
         '''Convert the audio signal from the time domain to the frequency domain.'''
         frame -= np.average(frame)
-        fft_normal = fft.fft(frame) / len(frame)
+        fft_normal = fft.fft(frame)
 
-        result = abs(fft_normal[range(len(frame) // 2)]).astype(np.float64)
-        result *= 1 / result.max(axis=0)
-        return result
+        return abs(fft_normal[:len(frame) // 2])
+
+    def normalize_frequencies(self, frequencies):
+        frequencies *= 1 / frequencies.max(axis=0)
+        return frequencies
 
     def start(self):
         pass
--- vpt/sources/wav_audio_source/__init__.py
+++ vpt/sources/wav_audio_source/__init__.py
@@ -2,43 +2,21 @@
 
 import math
 
+import matplotlib.pyplot as plt
 import numpy as np
+import sounddevice as sd
+import soundfile as sf
 from rx import Observable
 from rx.subject import Subject
-from scipy.io import wavfile
+from scipy.fft import fft
 
 from vpt.sources.base import SourceBase
 
 
-from scipy.fft import fft
-import numpy as np
-from numpy import arange
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-import os
-
-def frequency_sepectrum(x, sf):
-    """
-    Derive frequency spectrum of a signal from time domain
-    :param x: signal in the time domain
-    :param sf: sampling frequency
-    :returns frequencies and their content distribution
-    """
-    x = x - np.average(x)  # zero-centering
-
-    n = len(x)
-    k = arange(n)
-    tarr = n / float(sf)
-    frqarr = k / float(tarr)  # two sides frequency range
-
-    frqarr = frqarr[range(n // 2)]  # one side frequency range
-
-    x = fft(x) / n  # fft computing and normalization
-    x = abs(x[range(n // 2)]).astype(np.float64)
-    x *= 1 / x.max(axis=0)
-
-
-    return frqarr, x
+def freq_decompose(signal, sample_rate):
+    signal -= np.average(signal)
+    fft_norm = fft(signal)
+    return abs(fft_norm[:len(fft_norm) // 2]).astype(np.float64)
 
 
 class WavAudioSource(SourceBase[np.ndarray]):
@@ -46,42 +24,52 @@ class WavAudioSource(SourceBase[np.ndarray]):
     subj = Subject()
     filename: str
     sample_rate: int
+    debug: bool
 
-    def __init__(self, filename):
+    def __init__(self, filename, debug=False):
         '''Select the WAV file to read from.'''
         self.filename = filename
+        self.debug = debug
 
     def get_data_stream(self) -> Observable:
         return self.subj
 
     def start(self):
         '''Outputs the entire file into the stream in 1-second chunks.'''
-        sf, data = wavfile.read(self.filename)
-        print(sf)
+        data, sample_rate = sf.read(self.filename)
         if data.ndim == 1:
             data = data.reshape(len(data), 1)
-        self.sample_rate = sf
-        for chunk in np.array_split(data, math.ceil(len(data) / sf)):
+        self.sample_rate = sample_rate
+
+        if self.debug:
+            print(f'Sample rate: {self.sample_rate}')
+            print(f'Total samples: {data.shape}')
+
+        for chunk in np.array_split(data, math.ceil(len(data) / sample_rate)):
             self.subj.on_next(chunk)
+            if self.debug:
+                sd.play(chunk, self.sample_rate)
+                sd.wait()
+                self.plot_chunk(chunk)
+                plt.show()
 
-            y = chunk[:, 0]  # use the first channel (or take their average, alternatively)
-            t = np.arange(len(y)) / float(sf)
+    def plot_chunk(self, chunk):
+        mono = chunk.mean(axis=1)
+        time = np.arange(len(mono)) / float(self.sample_rate)
 
-            plt.figure()
-            plt.subplot(2, 1, 1)
-            plt.plot(t, y)
-            plt.xlabel('t')
-            plt.ylabel('y')
+        plt.figure()
+        plt.subplot(2, 1, 1)
+        plt.plot(time, mono)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
 
-            frq, X = frequency_sepectrum(y, sf)
+        amplitudes = freq_decompose(mono, self.sample_rate)
 
-            plt.subplot(2, 1, 2)
-            plt.plot(frq, X, 'b')
-            plt.xlabel('Freq (Hz)')
-            plt.ylabel('|X(freq)|')
-            plt.tight_layout()
-
-            plt.show()
+        plt.subplot(2, 1, 2)
+        plt.plot(np.arange(len(amplitudes)), amplitudes, 'b')
+        plt.xlabel('Freq (Hz)')
+        plt.ylabel(f'Amplitude')
+        plt.tight_layout()
 
     def stop(self):
         pass
