870ff104f71232f8b24ccf7838cea0120028fe83
Lev Chelyadinov <leva181777@gmail.com>
2021-02-26 17:12:27 +0500

    Set up basic frequency analysis

--- vpt/processors/speech_detector/__init__.py
+++ vpt/processors/speech_detector/__init__.py
@@ -1,6 +1,47 @@
 '''Processes an audio stream to detect speech.'''
+
+import numpy as np
+from rx.subject import Subject
+from scipy import fft
+
+from vpt.sources.base import SourceBase
 from vpt.processors.base import ProcessorBase
 
+# The frequency range of human voice in Hz.
+VOICE_RANGE = range(80, 1100 + 1)
+# The threshold which the amplitude needs to overcome to be considered present (in undefined units).
+NOISE_THRESHOLD = .5
 
-class SpeechDetector(ProcessorBase):
+
+class SpeechDetector(ProcessorBase[bool]):
     '''Detects speech in an audio stream and outputs a signal stream for that.'''
+    subj = Subject()
+
+    def __init__(self, audio_source: SourceBase[np.ndarray]):
+        '''Wire up the detector to an arbitrary audio source.'''
+        audio_source.get_data_stream().subscribe(self.detect_speech)
+
+    def get_data_stream(self):
+        return self.subj
+
+    def detect_speech(self, frame: np.ndarray):
+        '''Detect sound in speech frequencies in a given audio frame.'''
+        if frame.ndim == 1:
+            frame = frame.reshape(len(frame), 1)
+        self.subj.on_next(np.any(self.fft(frame.mean(axis=1))[VOICE_RANGE] >= NOISE_THRESHOLD))
+
+    @staticmethod
+    def fft(frame):
+        '''Convert the audio signal from the time domain to the frequency domain.'''
+        frame -= np.average(frame)
+        fft_normal = fft.fft(frame) / len(frame)
+
+        result = abs(fft_normal[range(len(frame) // 2)]).astype(np.float64)
+        result *= 1 / result.max(axis=0)
+        return result
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
--- vpt/sources/__init__.py
+++ vpt/sources/__init__.py
@@ -1,5 +1,6 @@
 '''A collection of data source nodes.'''
 from .device_audio_source import DeviceAudioSource
+from .wav_audio_source import WavAudioSource
 from .device_video_source import DeviceVideoSource
 from .keyboard_source import KeyboardSource
 from .mouse_source import MouseSource
