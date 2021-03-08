b786663d2ecec5ce8ccab2ca3ae84176b05751b2
Lev Chelyadinov <leva181777@gmail.com>
2021-03-08 17:12:57 +0300

    Refactor the sinks to use the new interface

--- vpt/capabilities/input_capable.py
+++ vpt/capabilities/input_capable.py
@@ -2,10 +2,24 @@
 from abc import ABC
 from typing import List
 
-from .output_capable import OutputCapable
+from .output_capable import OutputCapable, Initiatable
 
 
-class InputCapable(ABC):
+class InputCapable(Initiatable, ABC):
     '''Base class for input-capable nodes.
        Should be used for processors and sinks.'''
     sources: List[OutputCapable]
+
+    def start(self):
+        if not self.stopped:
+            return
+        self.stopped = False
+        for source in self.sources:
+            source.start()
+
+    def stop(self):
+        if self.stopped:
+            return
+        self.stopped = True
+        for source in self.sources:
+            source.stop()
--- vpt/processors/base.py
+++ vpt/processors/base.py
@@ -1,6 +1,6 @@
 '''The base interfaces for the processor nodes.'''
 from abc import ABC
-from typing import TypeVar, List
+from typing import TypeVar
 
 from ..capabilities import InputCapable, OutputCapable
 
@@ -9,18 +9,3 @@ T = TypeVar('T')
 
 class ProcessorBase(OutputCapable[T], InputCapable, ABC):
     '''Should be used for intermediary processing nodes.'''
-    sources: List[OutputCapable]
-
-    def start(self):
-        if not self.stopped:
-            return
-        self.stopped = False
-        for source in self.sources:
-            source.start()
-
-    def stop(self):
-        if self.stopped:
-            return
-        self.stopped = True
-        for source in self.sources:
-            source.stop()
