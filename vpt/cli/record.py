1e3494373d657007704414feeca2a925c4137d17
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-03-10 12:13:38 +0300

    Store the CSVs into a data directory

--- vpt/cli/record.py
+++ vpt/cli/record.py
@@ -6,7 +6,7 @@ from typing import Union
 
 import sounddevice as sd
 from rx.scheduler.mainloop import QtScheduler
-from matplotlib.backends.qt_compat import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5 import QtCore, QtWidgets
 
 from vpt.processors import EngagementEstimator, GazeDetector, SpeechDetector, MouseCompressor
 from vpt.processors.video_engagement_estimator import VideoEngagementEstimator
@@ -34,17 +34,13 @@ def record(audio_source: Union[str, int], video_source_id: int):
     engagement_estimator = EngagementEstimator(video_estimator,
                                                speech_detector)
 
-    store = SQLiteStore(f'session-{datetime.now().strftime("%m-%d-%H-%M")}.db',
+    store = SQLiteStore(f'data/session-{datetime.now().strftime("%m-%d-%H-%M")}',
                         mouse_compressor,
                         keyboard_source,
                         engagement_estimator)
     graph_view = GraphView(mouse_compressor,
                            keyboard_source,
                            engagement_estimator)
-
-    mouse_compressor.output.subscribe(lambda *x: store.update())
-    keyboard_source.output.subscribe(lambda *x: store.update())
-    engagement_estimator.output.subscribe(lambda *x: store.update())
 
     qapp = QtWidgets.QApplication.instance()
     if not qapp:
